c2431c23df72dca2852dcd88fe11e3877d31ce42
Gabriel Alvarado <a.22308051281127@cbtis128.edu.mx>
2024-11-20 02:07:01 -0600

    Add files via upload

--- UIII_P1_Tabla_Materia_CRUD_v4_Tablas/sucursales_app/urls.py
+++ UIII_P1_Tabla_Materia_CRUD_v4_Tablas/sucursales_app/urls.py
@@ -3,9 +3,8 @@ from sucursales_app import views
 
 urlpatterns = [
     path("", views.inicio_vista, name="inicio_vista"),
-    path("registrarMateria/",views.registrarMateria,name="registrarMateria"),
-    path('seleccionarMateria/<codigo>', views.seleccionarMateria, name='seleccionarMateria'),
-    path('editarMateria/', views.editarMateria, name='editarMateria'),
-    path('borrarMateria/<codigo>', views.borrarMateria, name='borrarMateria' ),
+    path("registrarSucursal/",views.registrarSucursal,name="registrarSucursal"),
+    path('seleccionarSucursal/<codigo>', views.seleccionarSucursal, name='seleccionarSucursal'),
+    path('editarSucursal/', views.editarSucursal, name='editarMateria'),
+    path('borrarSucursal/<codigo>', views.borrarSucursal, name='borrarSucursal' ),
 ]
-
--- UIII_P1_Tabla_Materia_CRUD_v4_Tablas/sucursales_app/views.py
+++ UIII_P1_Tabla_Materia_CRUD_v4_Tablas/sucursales_app/views.py
@@ -1,3 +1,49 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from .models import sucursales
 # Create your views here.
+def inicio_vista(request):
+    sucursal=sucursales.objects.all()
+    return  render(request,"gestionarsucursales.html",{"misucursal":sucursal})
+
+def registrarSucursal(request):
+    id_sucursal=request.POST["txtid"]
+    nombre=request.POST["txtnombre"]
+    Direccion=request.POST["txtdireccion"]
+    NumTelefono=request.POST["NumTel"]
+    creditos=request.POST["txtcreditos"]
+    correo=request.POST["txtcorreo"]
+    Horario=request.POST["txthorario"]
+
+    guardarSucursal=sucursales.objects.create(
+    id_sucursal=id_sucursal, nombre=nombre, Direccion=Direccion, NumTelefono=NumTelefono,
+    creditos=creditos, correo=correo, Horario=Horario)
+    return redirect("/")
+
+def seleccionarSucursal(request, id_sucursal):
+    sucursal = sucursales.objects.get(id_sucursal=id_sucursal)
+    return render(request,'editarsucursales.html', {'misucursal': sucursal})
+
+
+def editarSucursal(request):
+    id_sucursal=request.POST['txtcodigo']
+    nombre=request.POST['txtnombre']
+    Direccion=request.POST["txtdireccion"]
+    NumTelefono=request.POST["NumTel"]
+    creditos=request.POST["txtcreditos"]
+    correo=request.POST["txtcorreo"]
+    Horario=request.POST["txthorario"]
+    sucursal = sucursales.objects.get(id_sucursal=id_sucursal)
+    sucursal.nombre = nombre
+    sucursal.Direccion = Direccion
+    sucursal.NumTelefono = NumTelefono
+    sucursal.creditos = creditos
+    sucursal.correo = correo
+    sucursal.Horario = Horario
+    sucursal.save() # guarda registro actualizado
+    return redirect("/")
+
+
+def borrarSucursal(request, id_sucursal):
+    sucursal = sucursales.objects.get(id_sucursal=id_sucursal)
+    sucursal.delete() # borra el registro
+    return redirect("/")
